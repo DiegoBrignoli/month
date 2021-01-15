03b689bd558e7a51c179ff3d429cf3008c091c94
DiegoBrignoli <75189655+DiegoBrignoli@users.noreply.github.com>
2021-01-15 18:00:53 +0100

    Add files via upload

---
+++ month.go
@@ -0,0 +1,60 @@
+package month  //tutti e 4
+
+type Month int
+
+const (
+	Gennaio Month = 1 + iota
+	Febbraio
+	Marzo
+	Aprile
+	Maggio
+	Giugno
+	Luglio
+	Agosto
+	Settembre
+	Ottobre
+	Novembre
+	Dicembre
+)
+
+var longMonthNames = []string{
+	"Gennaio",
+	"Febbraio",
+	"Marzo",
+	"Aprile",
+	"Maggio",
+	"Giugno",
+	"Luglio",
+	"Agosto",
+	"Settembre",
+	"Ottobre",
+	"Novembre",
+	"Dicembre",
+}
+
+// String returns the English name of the month ("January", "February", ...).
+func (m Month) String() string {
+	if Gennaio <= m && m <= Dicembre {
+		return longMonthNames[m-1]
+	}
+	buf := make([]byte, 20)
+	n := fmtInt(buf, uint64(m))
+	return "%!Month(" + string(buf[n:]) + ")"
+}
+
+// fmtInt formats v into the tail of buf.
+// It returns the index where the output begins.
+func fmtInt(buf []byte, v uint64) int {
+	w := len(buf)
+	if v == 0 {
+		w--
+		buf[w] = '0'
+	} else {
+		for v > 0 {
+			w--
+			buf[w] = byte(v%10) + '0'
+			v /= 10
+		}
+	}
+	return w
+}
